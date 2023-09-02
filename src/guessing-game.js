157c7167d35868ccd73e24cf3ae587d8415bda3c
Rekips <rekips8@gmail.com>
2023-09-02 19:48:08 +0600

    feat: update guessing game src

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
+
+let minNum = 0, maxNum = 0, g = 0;
 class GuessingGame {
     constructor() {}
-
     setRange(min, max) {
-
+        minNum = min;
+        maxNum = max;
     }
 
     guess() {
-
+        g = Math.round(minNum + (maxNum - minNum) / 2);
+        return g;
     }
 
     lower() {
-
+        maxNum = g;
     }
 
     greater() {
-
+        minNum = g;
     }
 }
 
